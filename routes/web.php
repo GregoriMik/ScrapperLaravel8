929d93ea5f40ad2daa3bf213401eb6c476231405
Gregori Mikina <gregorimikina@gmail.com>
2021-06-18 11:16:06 +0200

    Create scraper routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('scraper',[App\Http\Controllers\ScraperController::class,'scraper'])->name('scraper');
