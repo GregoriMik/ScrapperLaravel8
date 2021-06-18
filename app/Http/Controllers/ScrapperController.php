ea8c4a02eaf00c2391015b117fe364767cf46133
Gregori Mikina <gregorimikina@gmail.com>
2021-06-18 11:30:09 +0200

    Get page coronavirus

--- app/Http/Controllers/ScrapperController.php
+++ app/Http/Controllers/ScrapperController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Goutte\Client;
 
 class ScrapperController extends Controller
 {
+    private $result = array();
+
     public function scraper(){
+        
+        $client = new Client();
+        $url='https://www.worldometers.info/coronavirus/';
+        $page=$client->request('GET',$url);
+
+        echo"<pre>";
+        print_r($page);
+
         return view('scraper');
     }
 }
